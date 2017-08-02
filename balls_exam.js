799ba6539ec5767bd9acc10c1fc42fe3ff6a5b44
Bohdan Levantovych <bohdan.levantovych@eleks.com>
2017-08-02 18:34:24 +0300

    add results

--- balls_exam.js
+++ balls_exam.js
@@ -1,19 +1,46 @@
 const _ = require('lodash');
 
-const sampleSize = 1000000;
+const sampleSize = 10000000;
 const blackBalls = _.times(12, _.constant('w'))
 const whiteBalls = _.times(8, _.constant('b'))
 const allBalls = _.shuffle([...whiteBalls, ...blackBalls])
 
-// console.log(allBalls);
-const allSamples = []
+/////////////////////////
+// 
+// WITHOUT RETURN
+//
+/////////////////////////
 
-for (let i = 0; i < sampleSize; i++) {
-    allSamples.push(_.sampleSize(allBalls, 2))
-    // console.log();
-}
-const differentBalls = allSamples.filter(sample => new Set(sample).size !== 1)
-console.log(differentBalls.length);
+function probabilityWithoutReturn() {
+    const allSamples = []
+    for (let i = 0; i < sampleSize; i++) {
+        allSamples.push(_.sampleSize(allBalls, 2))
+    }
+    const differentBalls = allSamples.filter(sample => new Set(sample).size !== 1)
+    return differentBalls.length / sampleSize
+}   
 
-console.log(differentBalls.length / sampleSize);
-console.log(12 / 25);
+
+/////////////////////////
+// 
+// WITH RETURN
+//
+/////////////////////////
+
+function probabilityWithReturn() {
+    const allSamples = []
+    for (let i = 0; i < sampleSize; i++) {
+        allSamples.push([
+            allBalls[_.random(0, allBalls.length - 1)],
+            allBalls[_.random(0, allBalls.length - 1)]
+        ])
+    }
+    const differentBalls = allSamples.filter(sample => new Set(sample).size !== 1)
+    return differentBalls.length / sampleSize
+}   
+console.log(probabilityWithoutReturn());
+console.log(2 * 8 / 20 * 12 / 19); // with return
+
+console.log(probabilityWithReturn());
+console.log(2 * 8 / 20 * 12 / 20 ); // with return
+
